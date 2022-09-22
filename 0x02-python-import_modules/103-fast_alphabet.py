dbaa5ea7d4749c2650797c3cf7cf3a64b8fb71b6
mulukenabay <mule.andisew@gmail.com>
2022-09-22 08:17:35 -0700

    103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
 import string
- 
-# initialize z
-z = 20
- 
-# printing z
-print(& quot
-       Number of elements required : & quot
-       + str(z))
- 
-# Print Alphabets till z
-# Using string.ascii_lowercase + slicing
-res = string.ascii_lowercase[:z]
- 
-# printing result
-print(& quot
-       Alphabets till z are : & quot
-       + str(res))
+print(string.ascii_uppercase)

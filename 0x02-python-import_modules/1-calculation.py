49c29ca7980369bb52932d3c1a3993cb135e7e58
mulukenabay <mule.andisew@gmail.com>
2022-09-22 08:26:10 +0000

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}" .format(a, b, a+b))
+
+add(10, 5)
+
+def sub(a, b):
+    a = 10
+    b = 5
+    print("{} - {} = {}" .format(a, b, a-b))
+
+sub(10, 5)
+
+def mul(a, b):
+    a = 10
+    b = 5
+    print("{} * {} = {}" .format(a, b, a*b))
+
+mul(10, 5)
+
+def div(a, b):
+    a = 10
+    b = 5
+    print("{} / {} = {}" .format(a, b, a/b))
+
+div(10, 5)

0739ee175b6fa1afc43783545eeb86b6ef463f2a
mulukenabay <mule.andisew@gmail.com>
2022-09-24 02:09:30 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    s = my_string 
-    print(s.translate({ord(i): None for i in 'cC'}))
+    s = my_string.translate({ord(i): None for i in 'cC'}))
+    return s

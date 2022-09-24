8c1c2655be885667060c144a525c5766b86e5140
mulukenabay <mule.andisew@gmail.com>
2022-09-24 05:06:08 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    large_number = my_list[0]
+    for number in my_list:
+        if len(my_list == 0:
+            my_list = 0, "None"
+        if number > large-number:
+            large_number = number
+        return large_number

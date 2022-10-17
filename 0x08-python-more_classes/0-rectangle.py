2838750e789348e1a37d178c20ad24fb855a2803
mulukenabay <mule.andisew@gmail.com>
2022-10-17 05:01:36 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a class that defines a rectangle """
+class Rectangle:
+    """ a class defines a rectangle """
+    def __init__(self, size):
+        """ initializes arectangle
+        Args:
+            a - the length of the rectangle
+            b - the width of the rectangle
+         """
+        return a * b
+
+

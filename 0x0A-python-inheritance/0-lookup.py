951bd51ed744a30d65d515ac1a720c2f530ce814
mulukenabay <mule.andisew@gmail.com>
2022-10-24 08:04:30 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+''' a python class with one method whic returns a list'''
+class Myclass():
+    pass
+    '''class which returns a list of all instance attributes and methods'''
+    def __init__(self):
+        pass
+    def lookup(obj):
+        return dir()
+
+object = Myclass()
+object.lookup()

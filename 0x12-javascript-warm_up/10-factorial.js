41a7f1c3d6fab64c60006c280bf0e73ea5654132
mulukenkebede <106770288+mulukenkebede@users.noreply.github.com>
2022-12-20 21:27:23 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,15 @@
 #!/usr/bin/node
-const x = process.argv[2];
-function factorial (x) {
-  if (isNaN(x) || x === 1) {
+// script that computes and prints a factorial recursively
+const arg = process.argv.slice(2);
+const argInt = parseInt(arg[0]);
+function factorial (n) {
+  if (isNaN(n)) {
+    return (1);
+  } else if (n === 1) {
     return (1);
   } else {
-    return (x * factorial(x - 1));
+    return (n * factorial(n - 1));
   }
 }
-console.log(factorial(parseInt(x)));
+
+console.log(factorial(argInt));
